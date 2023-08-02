2cb4da3b953a978920b3c09d050219ad30f57273
Leonardo Fragozo <leo.fragozo.dev@gmail.com>
2023-08-02 18:34:13 -0300

    Fix variable name lol

--- irregular_verb.rb
+++ irregular_verb.rb
@@ -2,37 +2,37 @@ require_relative 'word'
 
 class IrregularVerb < Word
   def conjugate
-    iku_steam = word[-1] == "く" ? word[0...-1] : word[0]
+    iku_stem = word[-1] == "く" ? word[0...-1] : word[0]
 
     conjugations = {
       "Present" => word,
-      "Past" => "#{iku_steam}った",
-      "Negative" => "#{iku_steam}かない",
-      "Past negative" => "#{iku_steam}かなかった",
-      "Polite present" => "#{iku_steam}きます",
-      "Polite past" => "#{iku_steam}きました",
-      "Polite negative" => "#{iku_steam}ません",
-      "Polite past negative" => "#{iku_steam}ませんでした",
-      "たら form" => "#{iku_steam}ったら",
-      "たら negative" => "#{iku_steam}なかったら",
-      "ば form" => "#{iku_steam}けば",
-      "ば negative" => "#{iku_steam}けなければ",
-      "Conjunctive" => "#{iku_steam}って",
-      "Negative conjunctive" => "#{iku_steam}かなくて",
-      "Without doing" => "#{iku_steam}かないで",
-      "'Formal' without doing" => "#{iku_steam}かずに",
-      "Volitional" => "#{iku_steam}こう",
-      "Polite volitional" => "#{iku_steam}きましょう",
-      "Negative 'formal' volitional" => "#{iku_steam}くまい",
-      "First person desire" => "#{iku_steam}きたい",
-      "Third person desire" => "#{iku_steam}きたがる",
-      "Potential" => "#{iku_steam}ける",
-      "Passive" => "#{iku_steam}かられる",
-      "Causative" => "#{iku_steam}かさせる",
-      "Passive causative" => "#{iku_steam}かさせられる",
-      "Imperative" => "#{iku_steam}け",
-      "Negative imperative" => "#{iku_steam}くな",
-      "Old negative" => "#{iku_steam}かぬ / #{iku_steam}かん"
+      "Past" => "#{iku_stem}った",
+      "Negative" => "#{iku_stem}かない",
+      "Past negative" => "#{iku_stem}かなかった",
+      "Polite present" => "#{iku_stem}きます",
+      "Polite past" => "#{iku_stem}きました",
+      "Polite negative" => "#{iku_stem}ません",
+      "Polite past negative" => "#{iku_stem}ませんでした",
+      "たら form" => "#{iku_stem}ったら",
+      "たら negative" => "#{iku_stem}なかったら",
+      "ば form" => "#{iku_stem}けば",
+      "ば negative" => "#{iku_stem}けなければ",
+      "Conjunctive" => "#{iku_stem}って",
+      "Negative conjunctive" => "#{iku_stem}かなくて",
+      "Without doing" => "#{iku_stem}かないで",
+      "'Formal' without doing" => "#{iku_stem}かずに",
+      "Volitional" => "#{iku_stem}こう",
+      "Polite volitional" => "#{iku_stem}きましょう",
+      "Negative 'formal' volitional" => "#{iku_stem}くまい",
+      "First person desire" => "#{iku_stem}きたい",
+      "Third person desire" => "#{iku_stem}きたがる",
+      "Potential" => "#{iku_stem}ける",
+      "Passive" => "#{iku_stem}かられる",
+      "Causative" => "#{iku_stem}かさせる",
+      "Passive causative" => "#{iku_stem}かさせられる",
+      "Imperative" => "#{iku_stem}け",
+      "Negative imperative" => "#{iku_stem}くな",
+      "Old negative" => "#{iku_stem}かぬ / #{iku_stem}かん"
     }
 
     return conjugations
